0cf07d850464f528e99aff6b998301b6270dd8a5
alvinperkins <3287692178@qq.com>
2018-04-18 23:35:32 +0800

    update

--- hive/Cookbook/Date.sql
+++ hive/Cookbook/Date.sql
@@ -3,3 +3,26 @@ select hiredate -interval 5 day as hd_minus_5D
     from emp
   where deptno=10
 
+select datediff(day, allen_hd, ward_hd)
+    from (
+select hiredate as ward_hd
+      from emp
+   where ename = 'WARD'
+         ) x, 
+         (
+select hiredate as allen_hd
+    from emp
+where ename = 'ALLEN'
+          ) y
+
+
+
+
+
+
+
+
+
+
+
+
